567f0b2c70b4abed7a28a5ee5ffec08b4c0bc0d2
Hamid Machfudin Sukardi <hamidmsukardi@gmail.com>
2023-08-14 02:00:06 +0700

    migration

---
+++ database/migrations/2023_08_13_183925_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('username')->unique();
+            $table->string('password');
+            $table->string('alamat');
+            $table->string('role');
+            $table->unsignedBigInteger('membership_id');
+            $table->foreign('membership_id')->references('id')->on('memberships');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
---
+++ database/migrations/2023_08_13_183940_create_transaction_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('transaksi_id');
+            $table->unsignedBigInteger('valas_id');
+            $table->decimal('rate', 10, 2);
+            $table->integer('qty');
+            $table->foreign('transaksi_id')->references('id')->on('transactions');
+            $table->foreign('valas_id')->references('id')->on('valas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_details');
+    }
+};
